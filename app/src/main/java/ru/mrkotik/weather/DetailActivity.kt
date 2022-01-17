4ffcc60262e091774b4ac79445d1945e7dd8774a
Andrey Mats <amats6655@gmail.com>
2022-01-18 00:29:29 +0300

    На коленке и коряво но засунул всё в TextView

--- app/src/main/java/ru/mrkotik/weather/DetailActivity.kt
+++ app/src/main/java/ru/mrkotik/weather/DetailActivity.kt
@@ -16,15 +16,52 @@ import java.util.ArrayList
 
 
 class DetailActivity : AppCompatActivity() {
-    private var day_1_00: TextView? = null
-    private var day_2_00: TextView? = null
+    private var day_1_03: TextView? = null
+    private var day_1_09: TextView? = null
+    private var day_1_15: TextView? = null
+    private var day_1_21: TextView? = null
+    private var day_2_03: TextView? = null
+    private var day_2_09: TextView? = null
+    private var day_2_15: TextView? = null
+    private var day_2_21: TextView? = null
+    private var day_3_03: TextView? = null
+    private var day_3_09: TextView? = null
+    private var day_3_15: TextView? = null
+    private var day_3_21: TextView? = null
+    private var day_4_03: TextView? = null
+    private var day_4_09: TextView? = null
+    private var day_4_15: TextView? = null
+    private var day_4_21: TextView? = null
+    private var day_5_03: TextView? = null
+    private var day_5_09: TextView? = null
+    private var day_5_15: TextView? = null
+    private var day_5_21: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        day_1_00 = findViewById(R.id.day_1_03)
-        day_2_00 = findViewById(R.id.day_2_03)
+        day_1_03 = findViewById(R.id.day_1_03)
+        day_1_09 = findViewById(R.id.day_1_09)
+        day_1_15 = findViewById(R.id.day_1_15)
+        day_1_21 = findViewById(R.id.day_1_21)
+        day_2_03 = findViewById(R.id.day_2_03)
+        day_2_09 = findViewById(R.id.day_2_09)
+        day_2_15 = findViewById(R.id.day_2_15)
+        day_2_21 = findViewById(R.id.day_2_21)
+        day_3_03 = findViewById(R.id.day_3_03)
+        day_3_09 = findViewById(R.id.day_3_09)
+        day_3_15 = findViewById(R.id.day_3_15)
+        day_3_21 = findViewById(R.id.day_3_21)
+        day_4_03 = findViewById(R.id.day_4_03)
+        day_4_09 = findViewById(R.id.day_4_09)
+        day_4_15 = findViewById(R.id.day_4_15)
+        day_4_21 = findViewById(R.id.day_4_21)
+        day_5_03 = findViewById(R.id.day_5_03)
+        day_5_09 = findViewById(R.id.day_5_09)
+        day_5_15 = findViewById(R.id.day_5_15)
+        day_5_21 = findViewById(R.id.day_5_21)
+
 
 
 
@@ -76,9 +113,27 @@ class DetailActivity : AppCompatActivity() {
 
 
 
-//                    day_1_00?.text = "123123"
-                    day_2_00?.text = JSONObject(apiResponse).getJSONArray("list").getJSONObject(1).toString()
-//                    day_1_00?.text = "$day $time:00 \n$desc \n $temp°, $speed м/с "
+                    day_1_03?.text = formatted_list.get(0)
+                    day_1_09?.text = formatted_list.get(1)
+                    day_1_15?.text = formatted_list.get(2)
+                    day_1_21?.text = formatted_list.get(3)
+                    day_2_03?.text = formatted_list.get(4)
+                    day_2_09?.text = formatted_list.get(5)
+                    day_2_15?.text = formatted_list.get(6)
+                    day_2_21?.text = formatted_list.get(7)
+                    day_3_03?.text = formatted_list.get(8)
+                    day_3_09?.text = formatted_list.get(9)
+                    day_3_15?.text = formatted_list.get(10)
+                    day_3_21?.text = formatted_list.get(11)
+                    day_4_03?.text = formatted_list.get(12)
+                    day_4_09?.text = formatted_list.get(13)
+                    day_4_15?.text = formatted_list.get(14)
+                    day_4_21?.text = formatted_list.get(15)
+                    day_5_03?.text = formatted_list.get(16)
+                    day_5_09?.text = formatted_list.get(17)
+                    day_5_15?.text = formatted_list.get(18)
+                    day_5_21?.text = formatted_list.get(19)
+
                 }
             }
         }
