929ab9a511ecb80459385284f834a00efae94ddd
Andrey Mats <amats6655@gmail.com>
2022-01-18 00:19:35 +0300

    Смог достать корректно данные из JSONArray, отформатировать и добавить в arraylist

--- app/src/main/java/ru/mrkotik/weather/DetailActivity.kt
+++ app/src/main/java/ru/mrkotik/weather/DetailActivity.kt
@@ -6,22 +6,25 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import org.jetbrains.anko.doAsync
+import org.json.JSONArray
 import org.json.JSONObject
 import java.net.URL
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
+import java.util.ArrayList
 
 
 class DetailActivity : AppCompatActivity() {
     private var day_1_00: TextView? = null
-
+    private var day_2_00: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        day_1_00 = findViewById(R.id.day_1_00)
+        day_1_00 = findViewById(R.id.day_1_03)
+        day_2_00 = findViewById(R.id.day_2_03)
 
 
 
@@ -32,39 +35,53 @@ class DetailActivity : AppCompatActivity() {
         super.onStart()
         val city = intent.getStringExtra("city")
         val key = intent.getStringExtra("key")
-        val url: String = "https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=$key&units=metric&lang=ru"
+        val url = "https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=$key&units=metric&lang=ru"
 
         doAsync {
             val apiResponse = URL(url).readText()
-            val dateformatter = DateTimeFormatter.ofPattern("yyyy-MM-DD HH:mm:ss")
-            var id = 0
-
-            val list = JSONObject(apiResponse).getJSONArray("list").getJSONObject(id)
-            val weather = list.getJSONArray("weather")
-            val main = list.getJSONObject("main")
-            val wind = list.getJSONObject("wind")
-
-            val dt = list.getString("dt_txt")
-            val day: String = LocalDate.parse(dt, dateformatter).dayOfMonth.toString() + " " + LocalDate.parse(dt, dateformatter).month.toString()
-            val time = LocalTime.parse(dt, dateformatter).hour.toString()
-            val desc = weather.getJSONObject(0).getString("description")
-            val temp = main.getString("temp")
-            val humidity = main.getString("humidity")
-            val speed = wind.getString("speed")
-            val gust = wind.getString("gust")
-
-
-
-
-
-
-
 
             synchronized(this){
-                runOnUiThread({
-                    day_1_00?.text = "$day $time:00 \n$desc \n $temp°, $speed м/с "
-                })
+
+                runOnUiThread {
+                    val dateformatter = DateTimeFormatter.ofPattern("yyyy-MM-DD HH:mm:ss")
+                    var id = 0
+
+// Преобразуем JSONArray в ArrayList
+
+                    var list: JSONArray = JSONObject(apiResponse).getJSONArray("list")
+                    val formatted_list = arrayListOf<String>()
+
+
+                    // Запихиваем вытащенные и отформатированные данные в formattedlist
+                    for (i in 0 until list.length()) {
+                        if (LocalTime.parse(list.getJSONObject(i).getString("dt_txt"), dateformatter).hour.toString() == "0" ||
+                            LocalTime.parse(list.getJSONObject(i).getString("dt_txt"), dateformatter).hour.toString() == "6" ||
+                            LocalTime.parse(list.getJSONObject(i).getString("dt_txt"), dateformatter).hour.toString() == "12" ||
+                            LocalTime.parse(list.getJSONObject(i).getString("dt_txt"), dateformatter).hour.toString() == "18"){
+                            continue
+                        }
+                        val weather = list.getJSONObject(i).getJSONArray("weather")
+                        val main = list.getJSONObject(i).getJSONObject("main")
+                        val wind = list.getJSONObject(i).getJSONObject("wind")
+
+                        val dt = list.getJSONObject(i).getString("dt_txt")
+                        val day = LocalDate.parse(dt, dateformatter).dayOfMonth.toString() + " " + LocalDate.parse(dt, dateformatter).month.toString()
+                        val time = LocalTime.parse(dt, dateformatter).hour.toString()
+                        val desc = weather.getJSONObject(0).getString("description")
+                        val temp = main.getString("temp")
+                        val speed = wind.getString("speed")
+                        formatted_list.add("$day $time:00 \n$desc \n$temp°, $speed м/с")
+                    }
+
+
+
+
+//                    day_1_00?.text = "123123"
+                    day_2_00?.text = JSONObject(apiResponse).getJSONArray("list").getJSONObject(1).toString()
+//                    day_1_00?.text = "$day $time:00 \n$desc \n $temp°, $speed м/с "
+                }
             }
         }
     }
 }
+
